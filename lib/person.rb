1ef1e110a60f1fe899959374372bcb02e537317e
Danny Brandt <dvbrandt90@gmail.com>
2021-01-13 13:54:52 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor :name, :height, :weight
   
   def initialize(arguments)
-    arguments.each{|key, value| self.send(("#{key}"=, value))}
+    arguments.each{|key, value| self.send(("#{key}="), value)}
   end 
   
 end
